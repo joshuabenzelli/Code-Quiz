20995320097be3cc32b8f0c0c8197fa0544229c4
Joshua Booth <joshuabenzelli@gmail.com>
2022-09-21 18:43:00 -0600

    Changed the readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Code-Quiz
+
+This is a little coding quiz to test your skills in a fun way! Every wrong answer detracts from your timer and at the end of the quiz, you'll be able to enter your initials and try and beat your score the next time! 
+
+I made this page using HTML, CSS, and Javascript.
+
+![Coding Quiz Screenshot](./Coding%20Quiz!%20-%20Google%20Chrome%209_20_2022%207_46_56%20AM.png)
--- script.js
+++ script.js
@@ -2,12 +2,17 @@ var startBtn = document.querySelector("#start");
 var header = document.querySelector(".headerClass");
 var desc = document.querySelector("#desc");
 var timerEl = document.getElementById("countdown");
+var timerState = timerEl.getAttribute("data-state");
 
 function start(){
     startBtn.style.display = "none";
     header.style.display = "none";
     desc.style.display = "none";
     var timer = 29;
+    if(timerState === "hidden") {
+        timerEl.textContent = "30 seconds remaining";
+        timerEl.dataset.state = "visible";
+    }
 
   var timeInterval = setInterval(function () {
     if (timer > 1) {
